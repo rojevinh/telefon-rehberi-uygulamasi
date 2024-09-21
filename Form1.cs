5f5bafda3c3afcc06d59a0215641b92a999a44b3
rojevinh <115618069+rojevinh@users.noreply.github.com>
2024-09-21 15:40:52 +0300

    c# form uygulaması ile basit bir telefon rehberi
    c# form uygulamasından tasarımını yapıp kısaca basit pratik amaçlı telefon rehberi

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+namespace telefon_rehberi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text + " " + textBox2.Text + " " + textBox3.Text);
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
